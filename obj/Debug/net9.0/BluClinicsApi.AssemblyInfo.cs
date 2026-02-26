80df7934cc88f33bf51f224638134597ea5e7270
Nukaiah04 <yalagalanukaiah14@gmail.com>
2026-02-26 13:40:58 +0530

    setup dockerfile and cloudbuild.yaml

--- obj/Debug/net9.0/BluClinicsApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BluClinicsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BluClinicsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6969caa80d9d10270a7d6075aee5403246680ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5d010e9edf32ad31fa896d6fa292c2c7bb8f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BluClinicsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BluClinicsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
